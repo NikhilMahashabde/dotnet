8aed8d153fcedb17dc2418cbde5dc5af728ed3d1
nikhil-mahashabde <nikhil.mahashabde@gmail.com>
2023-08-26 15:16:07 +1000

    Database

--- CodeFirstAspCore6/Controllers/HomeController.cs
+++ CodeFirstAspCore6/Controllers/HomeController.cs
@@ -1,22 +1,64 @@
 ﻿using CodeFirstAspCore6.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CodeFirstAspCore6.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        private readonly StudentDbContext studentDb;
+
+        public HomeController(StudentDbContext studentDb)
         {
-            _logger = logger;
+            this.studentDb = studentDb;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+
+            var stdData = await studentDb.Students.ToListAsync();
+            return View(stdData);
+        }
+
+        public IActionResult Create()
+        {
+
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                await studentDb.Students.AddAsync(std);
+                await studentDb.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            Console.WriteLine(std);
+
+            return View(std);
+        }
+
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+
+            var stdData = await studentDb.Students.FirstOrDefaultAsync(x => x.Id == id);
+            return View(stdData);
+        }
+
+
+
 
         public IActionResult Privacy()
         {
