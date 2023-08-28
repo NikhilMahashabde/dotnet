a0150fedf8f1718c6f2488f6eec4ea52df36f202
nikhil-mahashabde <nikhil.mahashabde@gmail.com>
2023-08-28 22:26:17 +1000

    Login form

---
+++ LoginFormAspCore6/LoginFormAspCore6/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using LoginFormAspCore6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoginFormAspCore6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MyDbContext myDbContext;
+
+        public HomeController(ILogger<HomeController> logger, MyDbContext myDbContext)
+        {
+            _logger = logger;
+            this.myDbContext = myDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+
+            var myUser = myDbContext.Users.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault();
+
+            if (myUser != null)
+            {
+                HttpContext.Session.SetString("id", myUser.Email);
+                return RedirectToAction("Dashboard");
+
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed..";
+            }
+
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            if (HttpContext.Session.GetString("id") != null)
+            {
+
+                ViewBag.MySession = HttpContext.Session.GetString("id").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("id") != null)
+            {
+
+                HttpContext.Session.Remove("id");
+                return RedirectToAction("Login");
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoginFormAspCore6/LoginFormAspCore6/Models/MyDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoginFormAspCore6.Models
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("gender");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
